211cd7160791fa9e4efeadce0da092b72e96fee8
Aredhele <vincentstehly@hotmail.fr>
2016-03-21 15:59:31 -0400

    Adding spatial gui text for chess playground

---
+++ Assets/scripts/LookAtTheCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAtTheCamera : MonoBehaviour {
+
+    public Camera m_cameraToLookAt;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+
+        // Force an element to face the specified camera
+        Vector3 v = m_cameraToLookAt.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt(m_cameraToLookAt.transform.position - v);
+        transform.Rotate(0, 180, 0);
+	}
+}
+
