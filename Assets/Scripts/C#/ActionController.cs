d67dbd48fb496f2cbea3ec3feb877b0e8be82078
Darenn Keller <keller.darenn@gmail.com>
2016-03-23 17:42:02 -0400

    Added an action controller script to manager the animation of a piece.

---
+++ Assets/Scripts/C#/ActionController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Will do animations and play sounds relative to the action done by the piece.
+/// Actions are Prepare, StopPrepare, Run, StopRun, Attack and Die.
+/// </summary>
+public class ActionController : MonoBehaviour {
+
+	// Audio clips to play
+	public AudioClip attackSound;
+	public AudioClip runSound;
+	public AudioClip dieSound;
+	public AudioClip prepareSound;
+
+	// Components
+	private AudioSource audioSource;
+	private Animator animator;
+
+	void Start() {
+		audioSource = gameObject.GetComponent<AudioSource> ();
+		animator = gameObject.GetComponent<Animator> ();
+	}
+
+	void Prepare() {
+		animator.SetBool ("IsPreparing", true);
+		audioSource.clip = prepareSound;
+		audioSource.Play ();
+	}
+
+	void StopPrepare() {
+		animator.SetBool ("IsPreparing", false);
+		audioSource.Stop ();
+	}
+
+	void Run() {
+		animator.SetBool ("IsMoving", true);
+		audioSource.clip = runSound;
+		audioSource.loop = true;
+		audioSource.Play ();
+	}
+
+	void StopRun() {
+		animator.SetBool ("IsMoving", false);
+		audioSource.loop = false;
+		audioSource.Stop ();
+	}
+
+	// Triggers
+
+	void Attack() {
+		animator.SetTrigger ("Attack");
+		audioSource.clip = attackSound;
+		audioSource.Play ();
+	}
+
+	void Die() {
+		animator.SetTrigger ("Die");
+		audioSource.clip = dieSound;
+		audioSource.Play ();
+	}
+
+	void Update() {
+
+	}
+}
