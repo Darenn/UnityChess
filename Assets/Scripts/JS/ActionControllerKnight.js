c2c22fd8a04afeed55b550a64b680e0b17cd1f74
Darenn Keller <keller.darenn@gmail.com>
2016-03-24 14:48:00 -0400

    Added animator for knight and jump aciton.

---
+++ Assets/Scripts/JS/ActionControllerKnight.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+public class ActionControllerKnight extends ActionController {
+
+	public var jumpSound : AudioClip;
+	public var landSound : AudioClip;
+	
+	private var isJumping : boolean = false;
+
+	function Jump() {
+		isJumping = true;
+		animator.SetTrigger("Jump");
+		audioSource.clip = jumpSound;
+		audioSource.Play ();
+	}
+	
+	function Land() {
+		isJumping = false;
+		animator.SetTrigger("Land");
+		audioSource.clip = landSound;
+		audioSource.Play ();
+	}
+
+}
