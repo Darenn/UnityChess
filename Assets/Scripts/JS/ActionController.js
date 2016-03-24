435dbbba0172bc665468826d9fc188da7b6e5af2
Darenn Keller <keller.darenn@gmail.com>
2016-03-24 10:27:56 -0400

    Added an ActionController in js for every character. Added somme sounds on the pawn actions.

---
+++ Assets/Scripts/JS/ActionController.js
@@ -0,0 +1,68 @@
+﻿#pragma strict
+
+import System;
+
+// Audio clips to play
+public var attackSound : AudioClip;
+public var runSound : AudioClip;
+public var dieSound : AudioClip;
+public var prepareSound : AudioClip;
+
+private var isRunning : boolean;
+
+// Components
+private var audioSource : AudioSource;
+private var animator : Animator;
+
+
+function Start() {
+	audioSource = gameObject.GetComponent(AudioSource);
+	animator = gameObject.GetComponent(Animator);
+}
+
+function Prepare() {
+	animator.SetBool ("IsPreparing", true);
+	audioSource.clip = prepareSound;
+	audioSource.Play ();
+}
+
+function StopPrepare() {
+	animator.SetBool ("IsPreparing", false);
+	audioSource.Stop ();
+}
+
+function Run() {
+	isRunning = true;
+	animator.SetBool ("IsMoving", true);
+	audioSource.clip = runSound;
+	audioSource.loop = true;
+	audioSource.Play ();
+}
+
+function StopRun() {
+	isRunning = false;
+	animator.SetBool ("IsMoving", false);
+	audioSource.loop = false;
+	audioSource.Stop ();
+}
+
+// Triggers
+
+function Attack() {
+	animator.SetTrigger ("Attack");
+	audioSource.clip = attackSound;
+	audioSource.Play ();
+}
+
+function Die() {
+	animator.SetTrigger ("IsAttacked");
+	audioSource.clip = dieSound;
+	audioSource.Play ();
+}
+
+// getters
+
+function IsRunning() : boolean {
+	return isRunning;
+}
+
