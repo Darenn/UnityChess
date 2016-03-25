e7e73f2fb5ee120e4b0d5ccd65ff041cb13ecb11
Aredhele <vincentstehly@hotmail.fr>
2016-03-25 10:54:54 -0400

    Adding documentation (highlighterController)

--- Assets/Scripts/JS/HighlighterController.js
+++ Assets/Scripts/JS/HighlighterController.js
@@ -1,30 +1,33 @@
 ﻿#pragma strict
 
 /**
- * TODO
+ * Handle the chessboard highlighting
  */
 public class HighlighterController extends MonoBehaviour
 {
 	public var highlighter : GameObject;
 
 	/**
-	 * TODO
+	 * Initialize the class
 	 */
 	function Start () {
 		// None
 	}
 
 	/**
-	 * TODO
+	 * Called once per frame
 	 */
 	function Update () {
-
+		// None
 	}
 
 	/**
-	 * TODO
+	 * Set the position of the highlighter plane on the scene
+	 *
+	 * @param position The supposed position of the plane (raycast resutlt)
+	 * @return void
 	 */
-	public function SetHighlighterPosition(position : Vector3) {
+	public function SetHighlighterPosition(position : Vector3) : void {
 		highlighter.GetComponent(Rigidbody).transform.position.x = position.x;
 		highlighter.GetComponent(Rigidbody).transform.position.z = position.z;
 	}
